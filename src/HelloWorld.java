10af20828a4ee91b3c1b4adf0e3cdf3819151a2b
future <180002337@qq.com>
2021-10-09 23:44:40 +0800

    修改了HelloWorld:增加了System.out.println()

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * @AUTHOR FutureLight
+ * @DATE 2021/10/9 - 23:13 - 2021
+ */
+public class HelloWorld {
+    public static void main(String args[]) {
+        System.out.println("ok");
+        System.out.println("Good");
+    }
+}
